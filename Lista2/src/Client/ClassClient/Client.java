967bb39a567fdd904113416bebdc1523e5281c8c
Lucas Correa <lucas.dcorrea1@gmail.com>
2020-02-28 23:43:33 -0300

    Add class

--- Lista2/src/Client/ClassClient/Client.java
+++ Lista2/src/Client/ClassClient/Client.java
@@ -6,37 +6,84 @@ public class Client {
     private String Name;
     private float Balance;
 
+    //Construtores
     public Client(String numberAccount,
                         String numberAgency,
                         String name,
                         float balance){
-        this.NumberAccount = numberAccount;
-        this.NumberAgency = numberAgency;
-        this.Name = name;
-        this.Balance = balance;
-
+        this.setNumberAccount(numberAccount);
+        this.setNumberAgency(numberAgency);
+        this.setName(name);
+        this.setBalance(balance);
     };
+
     public Client(){
         this.NumberAccount = "Não tem conta";
         this.NumberAgency = "Não tem agencia";
         this.Name = "Não tem nome";
         this.Balance = 0;
-
     };
 
+    //setters
+    //set
     public void setNumberAccount(String numberAccount){
         if (numberAccount.length() == 8 && (numberAccount.charAt(6) == '-')) {
             this.NumberAccount = numberAccount;
-        }else {
-            System.out.println("Numero da conta inválida");
         }
+        else {
+            System.out.println("Numero da conta inválida");
+        };
     };
 
     public void setNumberAgency(String numberAgency){
         if (numberAgency.length() == 6 && (numberAgency.charAt(4) == '-')) {
             this.NumberAgency = numberAgency;
-        }else{
+        }
+        else {
             System.out.println("Numero da agencia inválida");
         };
+    };
+
+    public void setBalance(float balance){
+        if (balance >= 0);
+        this.Balance = balance;
+    };
+
+    public void setName(String name){
+        if (name.length() <= 30) {
+            this.Name = name;
+        }
+        else {
+            System.out.println("Nome invalido");
+        };
+    };
+
+    //Get
+    public String getName(){
+        return this.Name;
+    };
+    public String getNumberAccount(){
+        return this.NumberAccount;
+    };
+    public String getNumberAgency(){
+        return this.NumberAgency;
+    };
+    public float getBalance(){
+        return this.Balance;
+    };
+
+    //Metodos
+    public void withdraw(float x){
+        this.setBalance(this.Balance -= x);
+    };
+    public void deposit(float x){
+        this.setBalance(this.Balance += x);
+    };
+
+    public String getClient(){
+        return "Nome: " + this.Name
+                + "\n Numero da conta: " + this.NumberAccount
+                + "\n Numero da agencia: " + this.NumberAgency
+                + "\n Saldo: " + this.Balance;
     };
 };
--- Lista2/src/Client/Test/TestClient.java
+++ Lista2/src/Client/Test/TestClient.java
@@ -3,14 +3,22 @@ package Client.Test;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class TestClient extends Application {
+import Client.ClassClient.Client;
 
-    public static void main(String[] args) {
-        launch(args);
+import javax.swing.*;
+
+public class TestClient {
+
+    public static void main(String[] args){
+        //Iniciando variaveis
+        String name = JOptionPane.showInputDialog( "Informe seu nome " );
+        String numberAccount = JOptionPane.showInputDialog( "Informe o numero da conta " );
+        String numberAency = JOptionPane.showInputDialog( "Informe o numero da agencia " );
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    Client clien1 = new Client(
+            
+    )
 
-    }
+
 }
